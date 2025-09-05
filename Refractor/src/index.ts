24d124b42569bc7645435f54d842631cf36b8d29
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-09-05 23:45:59 +0530

    ko

---
+++ Refractor/src/index.ts
@@ -0,0 +1,13 @@
+class TaskManager {
+    createTask(name: string):void {
+      console.log(`Creating task: ${name}`);
+    }
+  
+  }
+  
+class Email {
+    sendEmail(to: string):void {
+      console.log(`Sending email to ${to}`);
+    }
+}  
+
