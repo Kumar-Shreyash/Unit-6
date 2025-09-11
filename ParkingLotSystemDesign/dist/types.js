bba5211df31e7f109063536ddfd41980a3b71abe
Kumar Shreyash <shreyash.sinha35@gmail.com>
2025-09-12 01:09:47 +0530

    ok

---
+++ ParkingLotSystemDesign/dist/types.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.VehicleType = void 0;
+var VehicleType;
+(function (VehicleType) {
+    VehicleType["CAR"] = "CAR";
+    VehicleType["TRUCK"] = "TRUCK";
+    VehicleType["BIKE"] = "BIKE";
+})(VehicleType || (exports.VehicleType = VehicleType = {}));
+//**
+// Closest
+// Random Parking */
